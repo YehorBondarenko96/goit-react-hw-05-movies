ab5a4c29fb4126d1184adaf5a365aaa4716a6429
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-01-10 12:41:32 +0200

    5.4.4-10.01.24

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,8 @@ export const App = () => {
         <Route path="cast" element={<Cast />} />
         <Route path="reviews" element={<Reviews />} /> 
         </Route>
-        </Route>    
+        <Route path="*" element={<Home />} /> 
+        </Route>   
     </Routes>
   );
 };
