65d06a07a3881d1d87ba71166b94d0a1a9490840
YehorBondarenko96 <135607282+YehorBondarenko96@users.noreply.github.com>
2024-01-10 10:57:34 +0000

    Deploying to gh-pages from @ YehorBondarenko96/goit-react-hw-05-movies@bd36f7dbb231cd891240a8741a6366b2fda2f442 🚀

---
+++ static/js/760.5e96c9e4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[760],{760:function(t,e,i){i.r(e);var n=i(87),r=i(184);e.default=function(t){var e=t.filmsList,i=t.location;return(0,r.jsx)(r.Fragment,{children:e&&(0,r.jsx)("ul",{children:e.map((function(t){return(0,r.jsx)("li",{children:(0,r.jsx)(n.rU,{to:"/movies/".concat(t.id),state:{from:i},children:t.title})},t.id)}))})})}}}]);
+//# sourceMappingURL=760.5e96c9e4.chunk.js.map
